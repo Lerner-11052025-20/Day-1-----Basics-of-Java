837501ddc0f2bc5430aa7733b38891d8a4059f85
Lerner-11052025-20 <deep.professional803@gmail.com>
2025-05-18 23:56:13 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,51 @@
+public class Datatypes
+{
+    public static void main(String args[])
+    {
+        //primitive Data Type
+        //byte short char int long float double boolean
+
+        //byte DATA TYPE
+        byte x = -128;
+        System.out.println("byte data type : " + x);
+
+        //short DATA TYPE
+        short x2 = 32767;
+        System.out.println(x2);
+        
+        //char data type        
+        char ch = 65;
+        System.out.println(ch);
+        char ch1 = 90;
+        System.out.println(ch1);
+        char ch2 = 'A';
+        System.out.println(ch2);
+
+        //int data type
+        int a = 1;
+        System.out.println(a);
+
+        //long data type
+        long big = 12345678900000l; // l or L both are equal over here
+        System.out.println(big);
+        
+        //float data type
+        float $f = 1.09876789F; // f or F both are equal over here
+        System.out.println($f); // 6 digit sudhi print karshe and 7th ma digit ma roundup thashe
+        $f = 34E3f ;
+        System.out.println($f);
+
+        //double data type
+        double d2 = 1.987897898600890d; // d or D both are equal over here
+        System.out.println(d2);
+        double $d1eep = 34E4d ;
+        System.out.println($d1eep);
+
+        //boolean data type
+        boolean flag = true;
+        System.out.println(flag);
+        flag = false;
+        System.out.println(flag);
+
+    }
+}
