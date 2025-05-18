8896d89a535e067899bf9ec78d9a04c8e5264f46
Lerner-11052025-20 <deep.professional803@gmail.com>
2025-05-19 00:01:17 +0530

    Create Output.java

---
+++ Output.java
@@ -0,0 +1,11 @@
+public class PrintOutput
+{
+    public static void main(String args[])
+    {
+        System.out.println("My name is DEEP");
+        System.out.print("I am Computer science student at the Nirma University\n");
+
+        //pattern print simply
+        System.out.println("*\n**\n***");
+    }
+}
